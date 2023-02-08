9447e5a3cb717457142d99936c293e60b149c110
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-08 21:53:06 +0200

    added pokemon

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,17 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
   const ul = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemon = data.results;
+      pokemon.forEach(p => {
+        const listItem = document.createElement("li");
+        listItem.innerText = p.name
+        ul.appendChild(listItem);
+      })
+    });
+  
+
 });
