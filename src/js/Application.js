10f4b25437487e3d00a41fea88eb6ab32e4dfb00
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-09 12:24:44 +0200

    Added lyrics to the beat

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,30 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
+    this._beat = new Beat();
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat.addListener(Beat.events.BIT, (event) => {
+      if (count < lyrics.length) {
+        const currentLyric = lyrics[count];
+        this._create(currentLyric)
+        count++;
+      }
+    })
 
-    document.querySelector(".main").appendChild(message);
+   
 
     this.emit(Application.events.READY);
   }
+
+  _create(lyric) {
+      const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyric;
+      document.querySelector(".main").appendChild(message);
+  }
+
+  
+
 }
